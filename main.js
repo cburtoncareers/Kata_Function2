4067c0da0e351c1698189bc6099cc14fd9b595d6
Nita <cburtoncareers@yahoo.com>
2020-06-03 17:01:46 -0400

    completed kata functions

--- main.js
+++ main.js
@@ -1,47 +1,83 @@
+//KATA 1- ADD FUNCTION
+
 function add (x, y) {
     return x+y;
 }
-console.log(add(9, 8));
+console.log(add(2, 4));
 
+
+//KATA 2- MULTIPLY FUNCTION
 
 function multiply(x, y) {
     let product = 0
     for(let counter = 0; counter < y; counter++){
-        product + x
-        product = add(product, x) // product + x
         
+        product = add(product, x) // product + x
+        //product initiates '0' from the start
+        //When the code executes, the product reassigns to 6
+        //The next run, the products assigns 12 and so on until the final product is 24!
     }
     return product
     
 }
 
-console.log(multiply(6, 9));
+console.log(multiply(6, 4));
 
+
+//KATA 3- POWER FUNCTION
 
 function power(x, n) {
-    let expo = 1
-    for(let counter = 0; counter < n; counter+= 1){
-        expo *= multiply(x, 1);    
+    let pwr = 1
+    for(let counter = 0; counter < n; counter++) {
+        pwr = multiply(pwr, multiply(x, 1))
+        //Same as Kata 2, the first run of code executes 2*2
+        //Code keeps running until the answer is 256!
     }
 
-    return expo
+    return pwr 
 }
 
 console.log(power(2, 8));
 
 
+//KATA 4- FACTORIAL FUNCTION
+  
+function factorial(n) {
 
-function containsVowel (string) {
-      return string.includes('a')
-         ||  string.includes('e')
-         ||  string.includes('i')
-         ||  string.includes('o')
-         ||  string.includes('u')
+    let fact = n
+     for(let counter = 1; counter < n; counter++) {
+
+        fact = multiply(fact, multiply(counter, 1))
+        //The first run of code MUST make the factorial answer '20'!
+
+    }
+
+    return fact
+   
 }
 
-    console.log(containsVowel('ass'))
-    console.log(containsVowel('effer'))
-    console.log(containsVowel('iss'))
-    console.log(containsVowel('ool'))
-    console.log(containsVowel('upid'))
-    console.log(containsVowel('wxyz'))
+console.log(factorial(5))
+
+
+
+
+//KATA BONUS- FIBONACCI FUNCTION
+
+function fibonacci(n) {
+    let num1 = 0;
+    let num2 = 1;
+    let total = num2;
+    for (counter = 0; counter < n; counter ++) {
+        total = add(total, add(num2, 1))
+        
+    }
+    return total;
+
+}
+console.log(fibonacci(8));
+
+
+
+
+
+//*Citing Desmond Obiri-Yeboah for tutoring me on Functions.
